4dbcc38b2067792ba143d9d016fa291298b530e6
NamJinWoo <cjsencks@naver.com>
2019-06-03 00:00:00 -0700

    magic

--- Magic 8 Ball/Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/Magic 8 Ball/ViewController.swift
@@ -16,7 +16,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        changeImageView()
+        changeImageView() 
     }
 
     @IBAction func askButton(_ sender: Any) {

f003d56aa84090c395904b442094e237b42076d6
NamJinWoo <cjsencks@naver.com>
2019-06-15 22:03:52 -0700

    Classes and Objects

---
+++ Classes and Objects/Classes and Objects/Car.swift
@@ -0,0 +1,15 @@
+//
+//  Car.swift
+//  Classes and Objects
+//
+//  Created by Jinwoo on 15/06/2019.
+//  Copyright © 2019 Jinwoo Nam. All rights reserved.
+//
+
+import Foundation
+
+class Car {
+    
+    var color = "Black"
+    var numberOfSeats = 5
+}
---
+++ Classes and Objects/Classes and Objects/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Classes and Objects
+//
+//  Created by Jinwoo on 15/06/2019.
+//  Copyright © 2019 Jinwoo Nam. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
